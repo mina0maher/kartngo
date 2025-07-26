78d00f0e5b1cdb6a907c15bb9628d3c7b204bb82
mina0maher <mahermena160@gmail.com>
2025-07-26 12:09:16 +0300

    add OrderItem model

---
+++ app/src/main/java/com/mina/kartngo/models/OrderItem.java
@@ -0,0 +1,23 @@
+package com.mina.kartngo.models;
+
+public class OrderItem {
+    private final Product product;
+    private int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
