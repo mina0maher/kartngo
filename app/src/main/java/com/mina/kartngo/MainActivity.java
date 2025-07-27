8ffea217ba3f00fb3671540ddcfac168607bca76
mina0maher <mahermena160@gmail.com>
2025-07-28 00:19:48 +0300

    force light theme and portrait

--- app/src/main/java/com/mina/kartngo/MainActivity.java
+++ app/src/main/java/com/mina/kartngo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mina.kartngo;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Window window = getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.soft_blue));
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
