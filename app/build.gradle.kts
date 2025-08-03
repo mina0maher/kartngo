273b2b67a49126bde4cb464a4885bd7c7a3ed32a
mina <mahermena160@gmail.com>
2025-08-03 14:59:42 +0300

    setup retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment:2.7.7")
     implementation ("androidx.navigation:navigation-ui:2.7.7")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 }
